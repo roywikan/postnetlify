a53145805e23b90f2db4c6cb2a230650768a2926
wee <5243434+roywikan@users.noreply.github.com>
2024-11-29 23:13:34 +0700

    Update readPosts.js

--- netlify/functions/readPosts.js
+++ netlify/functions/readPosts.js
@@ -3,13 +3,20 @@ const fetch = require('node-fetch');
 exports.handler = async () => {
   try {
     const NETLIFY_ACCESS_TOKEN = process.env.NET_TOKEN;
-    //nfp_J9noW6zg8YM1XFrUJF1Sa3DYUrbaoJvB8b90
-   // Ganti dengan ID form Netlify Anda
-    const formId = "673faec750f0a700080c6bac";
+    //NET_TOKEN  //nfp_J9noW6zg8YM1XFrUJF1Sa3DYUrbaoJvB8b90
+    if (!NETLIFY_ACCESS_TOKEN) {
+      throw new Error("NET_TOKEN environment variable is missing");
+    }
 
+    const formId = "673faec750f0a700080c6bac";
     const endpoint = `https://api.netlify.com/api/v1/forms/${formId}/submissions`;
 
-    const response = await fetch(`${endpoint}?access_token=${NETLIFY_ACCESS_TOKEN}`);
+    const response = await fetch(endpoint, {
+      headers: {
+        Authorization: `Bearer ${NETLIFY_ACCESS_TOKEN}`,
+      },
+    });
+
     if (!response.ok) {
       return {
         statusCode: response.status,
@@ -29,3 +36,5 @@ exports.handler = async () => {
     };
   }
 };
+
+
