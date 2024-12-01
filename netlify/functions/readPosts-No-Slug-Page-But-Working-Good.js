f23f7a849c0300c13015ef87294b7d4cf1a6b9d5
wee <5243434+roywikan@users.noreply.github.com>
2024-12-01 11:05:37 +0700

    Create readPosts-No-Slug-Page-But-Working-Good.js

---
+++ netlify/functions/readPosts-No-Slug-Page-But-Working-Good.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+
+exports.handler = async () => {
+  try {
+    const NETLIFY_ACCESS_TOKEN = process.env.NET_TOKEN;
+    //NET_TOKEN  //nfp_J9noW6zg8YM1XFrUJF1Sa3DYUrbaoJvB8b90
+    if (!NETLIFY_ACCESS_TOKEN) {
+      throw new Error("NET_TOKEN environment variable is missing");
+    }
+
+    const formId = "673faec750f0a700080c6bac";
+    const endpoint = `https://api.netlify.com/api/v1/forms/${formId}/submissions`;
+
+    const response = await fetch(endpoint, {
+      headers: {
+        Authorization: `Bearer ${NETLIFY_ACCESS_TOKEN}`,
+      },
+    });
+
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: "Failed to fetch form submissions" }),
+      };
+    }
+
+    const submissions = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(submissions),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
