e6465d5d02c4e7e83e6f999641e77a9b5f9a6232
wee <5243434+roywikan@users.noreply.github.com>
2024-12-01 11:23:07 +0700

    Update readPosts.js

--- netlify/functions/readPosts.js
+++ netlify/functions/readPosts.js
@@ -1,10 +1,9 @@
 const fetch = require('node-fetch');
 
-exports.handler = async (event, context) => {
+exports.handler = async () => {
   try {
     const NETLIFY_ACCESS_TOKEN = process.env.NET_TOKEN;
-     //NET_TOKEN  //nfp_J9noW6zg8YM1XFrUJF1Sa3DYUrbaoJvB8b90
-
+    //NET_TOKEN  //nfp_J9noW6zg8YM1XFrUJF1Sa3DYUrbaoJvB8b90
     if (!NETLIFY_ACCESS_TOKEN) {
       throw new Error("NET_TOKEN environment variable is missing");
     }
@@ -26,23 +25,9 @@ exports.handler = async (event, context) => {
     }
 
     const submissions = await response.json();
-
-    // Get slug from the query string
-    const { slug } = event.queryStringParameters;
-    
-    // Find the post with the matching slug
-    const post = submissions.find(submission => submission.data.slug === slug);
-
-    if (!post) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ error: `Post with slug '${slug}' not found.` }),
-      };
-    }
-
     return {
       statusCode: 200,
-      body: JSON.stringify(post),
+      body: JSON.stringify(submissions),
     };
   } catch (error) {
     return {
@@ -51,4 +36,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
