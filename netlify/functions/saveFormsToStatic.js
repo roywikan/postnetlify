11d46bc2ed18ac8cacfdacae8b9a8ecd0520b23f
wee <5243434+roywikan@users.noreply.github.com>
2024-12-15 13:56:34 +0700

    Update saveFormsToStatic.js

--- netlify/functions/saveFormsToStatic.js
+++ netlify/functions/saveFormsToStatic.js
@@ -5,8 +5,6 @@ const axios = require("axios");
 
 
 
-const templatePath = path.join(__dirname, "static-post-template.html");
-const template = fs.readFileSync(templatePath, "utf8");
 
 
 
@@ -66,6 +64,9 @@ exports.handler = async (event) => {
   try {
     const templatePath = path.join(__dirname, "static-post-template.html");
     const template = fs.readFileSync(templatePath, "utf8");
+
+
+
 
     submissions.forEach((submission, index) => {
       const slug = submission.data.slug || `submission-${index + 1}`; // Nama file dari slug
